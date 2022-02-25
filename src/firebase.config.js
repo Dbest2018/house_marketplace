047ddb2d501d5aca144e33b2c8b2dabb89d6c0c7
Shittu Ibrahim <shit2wice@gmail.com>
2022-02-25 00:03:16 +0000

    [refactor] Initial configuration

---
+++ src/firebase.config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAuqSgBCyoKRG_V3CwNhhuo9q6w3RjEUHQ",
+  authDomain: "house-marketplace-eb93d.firebaseapp.com",
+  projectId: "house-marketplace-eb93d",
+  storageBucket: "house-marketplace-eb93d.appspot.com",
+  messagingSenderId: "700687808269",
+  appId: "1:700687808269:web:3190c31a115bc8e1aa6244",
+};
+
+export const db = getFirestore();
