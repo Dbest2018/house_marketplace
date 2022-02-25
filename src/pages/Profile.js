936c6507ccf5244abffef5df9c86ce6813b02379
Shittu Ibrahim <shit2wice@gmail.com>
2022-02-25 19:52:39 +0000

    [feat] Logout user from profile page  to explore page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,14 +1,29 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { getAuth } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
-  const [user, setUser] = useState(null);
-
   const auth = getAuth();
-  useEffect(() => {
-    setUser(auth.currentUser);
-  }, []);
-  return user ? <h1>{user.displayName}</h1> : "Not Logged in";
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+
+  const logout = () => {
+    auth.signOut();
+    navigate("/");
+  };
+  return (
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type="button" className="logOut" onClick={logout}>
+          Logout
+        </button>
+      </header>
+    </div>
+  );
 };
 
 export default Profile;
