29e774269e187173349148f1923b0ea0145f0546
Shittu Ibrahim <shit2wice@gmail.com>
2022-02-28 23:13:51 +0000

    [refactor] Add dependencies in catergory component

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,4 +1,17 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+import Spinner from "../components/Spinner";
 
 const Category = () => {
   return <div>Category</div>;
