46727ea105a32c21458c14efe6a91e383db839ac
Shittu Ibrahim <shit2wice@gmail.com>
2022-03-02 22:10:26 +0000

    [feat] Add local state listing

--- src/pages/CreateListing.js
+++ src/pages/CreateListing.js
@@ -1,6 +1,21 @@
-import React from "react";
+import { useState } from "react";
 
 const CreateListing = () => {
+  const [listing, setListing] = useState({
+    type: "rent",
+    name: "",
+    bedrooms: 1,
+    bathrooms: 1,
+    parking: false,
+    furnished: false,
+    address: "",
+    offer: false,
+    regularPrice: 0,
+    discountedPrice: 0,
+    images: {},
+    latitude: 0,
+    longtitude: 0,
+  });
   return <div>CreateListing</div>;
 };
 
