de57732e8e7c70984c70ddd02762008b5a099ba7
Shittu Ibrahim <shit2wice@gmail.com>
2022-03-03 01:48:31 +0000

    [feat] Add logged user to listing state

--- src/pages/CreateListing.js
+++ src/pages/CreateListing.js
@@ -1,6 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import Spinner from "../components/Spinner";
 
 const CreateListing = () => {
+  const [loading, setLoading] = useState(false);
+  const [geolocationEnabled, setGeolocationEnabled] = useState(false);
   const [listing, setListing] = useState({
     type: "rent",
     name: "",
@@ -16,6 +21,30 @@ const CreateListing = () => {
     latitude: 0,
     longtitude: 0,
   });
+
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    if (isMounted) {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setListing((prevListing) => ({ ...prevListing, userRef: user.uid }));
+        } else {
+          navigate("/sign-in");
+        }
+      });
+    }
+
+    return () => {
+      isMounted.current = false;
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isMounted]);
+  if (loading) {
+    return <Spinner />;
+  }
   return <div>CreateListing</div>;
 };
 
