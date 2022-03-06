4d60b8b235c283a599bb71be3c5c858ef9a44252
Shittu Ibrahim <shit2wice@gmail.com>
2022-03-06 20:14:29 +0000

    [feat] Update contact landlord page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,7 +6,7 @@ import { toast } from "react-toastify";
 
 const Contact = () => {
   const [message, setMessage] = useState("");
-  const [landloard, setLandlord] = useState(null);
+  const [landlord, setLandlord] = useState(null);
   const [searchParams, setSearchParams] = useSearchParams();
 
   const params = useParams();
@@ -24,7 +24,50 @@ const Contact = () => {
     };
     getLandlord();
   }, [params.landlordId]);
-  return <div>Contact</div>;
+
+  const handleChange = (e) => {
+    setMessage(e.target.value);
+  };
+  return (
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">Contact Landlord</p>
+      </header>
+
+      {landlord !== null && (
+        <main>
+          <div className="contactLandlord">
+            <p className="landlordName">Contact {landlord?.name}</p>
+          </div>
+
+          <form className="messageForm">
+            <div className="messageDiv">
+              <label htmlFor="message" className="messageLabel">
+                Message
+              </label>
+              <textarea
+                name="message"
+                id="message"
+                className="textarea"
+                value={message}
+                onChange={handleChange}
+              ></textarea>
+            </div>
+
+            <a
+              href={`mailto:${landlord.email}?Subject=${searchParams.get(
+                "listingName"
+              )}&body=${message}`}
+            >
+              <button type="button" className="primaryButton">
+                Send Message
+              </button>
+            </a>
+          </form>
+        </main>
+      )}
+    </div>
+  );
 };
 
 export default Contact;
